36405f666d76a1406dbc7788847ba2e9f947aea3
Arturo Rivera <arturo.riverar97@gmail.com>
2022-04-11 17:22:00 -0500

    Add backspace logic
    The element containing the backspace button was selected to add an
    event listener click, which would call the new function deleteValue().
    
    This function erases the last digit or symbol added to the display. It
    receives the event and the display, then it erases the last element.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,20 @@
 let valor = "";
 
 function addValue(evento, value = valor, screen = display){
-    value += evento.target.classList[1]
-    screen.textContent += value
+    value = evento.target.classList[1];
+    screen.textContent += value;
     console.log(value);
+}
+
+function deleteValue(evento, screen = display){
+    positionToKeep = screen.textContent.length-1;
+    screen.textContent = screen.textContent.substring(0,positionToKeep);
 }
 
 const numero = document.querySelectorAll('.tecla');
 numero.forEach(valor => valor.addEventListener('click', addValue));
 
 const display = document.querySelector('.display');
+
+const backspace = document.querySelector('.backspace');
+backspace.addEventListener('click', deleteValue);
