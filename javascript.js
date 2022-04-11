d00ef8f475e0074479a25aafcd86b4eba349f57d
Arturo Rivera <arturo.riverar97@gmail.com>
2022-04-11 18:45:52 -0500

    Store value before operator
    The operator keys were added another class to recognize that they were
    added, which helps to begin the process to store the first value. This
    is done by means of the substring method, which allows to store the
    String before the operator.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,29 @@
 let valor = "";
 
 function addValue(evento, value = valor, screen = display){
+    let firstValue = 0;
+    let secondValue = 0;
+    let lengthValue;
+    let operator = evento.target.classList[2];
+
     value = evento.target.classList[1];
     screen.textContent += value;
-    console.log(value);
+
+    if(operator != 'tecla'){
+        lengthValue = screen.textContent.length-1;
+        firstValue = parseInt(screen.textContent.substring(0,lengthValue));
+    }
+
+    console.log(lengthValue);
 }
 
 function deleteValue(evento, screen = display){
     positionToKeep = screen.textContent.length-1;
     screen.textContent = screen.textContent.substring(0,positionToKeep);
+}
+
+function eraseAll(event, screen = display){
+    screen.textContent = "";
 }
 
 const numero = document.querySelectorAll('.tecla');
@@ -18,3 +33,6 @@ const display = document.querySelector('.display');
 
 const backspace = document.querySelector('.backspace');
 backspace.addEventListener('click', deleteValue);
+
+const CE = document.querySelector('.CE');
+CE.addEventListener('click', eraseAll);
