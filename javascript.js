cbab2f7e85a9408460782ce5693b234c07dd96bd
Arturo Rivera <arturo.riverar97@gmail.com>
2022-04-11 19:54:28 -0500

    Create functions to perform operations
    When the key "=" is pressed, the conditional within addValue will
    call the operation() function, which has a switch conditional to
    evaluate what operation to perfomr according to the class received
    by the key that provided the operator.
    
    There are 4 cases in the switch conditional, which are:
    
    *add
    *subtract
    *multiply
    *div
    
    Each of these functions displays the correct value on the calculator;
    however, when an extra symbol is pressed and erased the result
    yields NaN. This problem will be addressed from here on.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 let valor = "";
 let lengthFirstValu = 0;
 let lengthSecondValu = 0;
+let operatorGlobal = "";
+let firstValu = 0;
+let secondValu = 0;
 
-function addValue(evento, value = valor, screen = display, lengthFirstValue = lengthFirstValu, lengthSecondValue = lengthSecondValu){
-    let firstValue;
-    let secondValue;
+function addValue(evento, value = valor, screen = display, 
+    lengthFirstValue = lengthFirstValu, lengthSecondValue = lengthSecondValu, 
+    operator = operatorGlobal, firstValue = firstValu, secondValue = secondValu){
 
-    let operator = evento.target.classList[2];
+
+    let operatorRecognize = evento.target.classList[2];
 
     value = evento.target.classList[1];
 
@@ -14,16 +18,25 @@ function addValue(evento, value = valor, screen = display, lengthFirstValue = le
         lengthSecondValue = screen.textContent.length;
         lengthSecondValu = lengthSecondValue;
         secondValue = parseInt(screen.textContent.substring(lengthFirstValue+1));
+        secondValu = secondValue;
+
+        operation(operator, firstValue, secondValue);
+        return;
+
     }
 
     screen.textContent += value;
 
-    if(operator != 'tecla' && value != 'igual'){
+    if(operatorRecognize != 'tecla' && value != 'igual'){
         lengthFirstValue = screen.textContent.length-1;
         lengthFirstValu = lengthFirstValue;
         firstValue = parseInt(screen.textContent.substring(0,lengthFirstValue));
+        firstValu = firstValue;
+        operator = operatorRecognize;
+        operatorGlobal = operator;
     }
 
+    console.log(firstValue)
     console.log(secondValue);
 }
 
@@ -35,6 +48,51 @@ function deleteValue(evento, screen = display){
 function eraseAll(event, screen = display){
     screen.textContent = "";
 }
+
+function operation(operator, firstValue, secondValue){
+    switch(operator) {
+        case "mas":
+            add(firstValue, secondValue);
+            break;
+
+        case "menos":
+            subtract(firstValue, secondValue);
+            break;
+
+        case "mult":
+            multiply(firstValue, secondValue);
+            break;
+
+        case "division":
+            div(firstValue, secondValue);
+            break;
+    }
+}
+
+function add(a,b, screen = display) {
+    let result = a+b;
+    screen.textContent = result;
+    return a+b;
+  };
+
+function subtract(a,b, screen = display) {
+    let result = a-b;
+    screen.textContent = result;
+    return a-b;
+  };
+
+function multiply(a,b, screen = display) {
+    let result = a*b;
+    screen.textContent = result;
+    return a*b;
+  };
+
+function div(a,b, screen = display) {
+    let result = a/b;
+    screen.textContent = result;
+    return a/b;
+  };
+
 
 const numero = document.querySelectorAll('.tecla');
 numero.forEach(valor => valor.addEventListener('click', addValue));
